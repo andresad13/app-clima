5dac5f8eb739beb419fb7bf21ec4d4ca7e45358e
leonel alfonso <andresad13@gmail.com>
2021-07-14 22:11:05 -0500

    commit master

---
+++ app/src/main/java/com/andresad13/clima_prueba_bold/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andresad13.clima_prueba_bold
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
